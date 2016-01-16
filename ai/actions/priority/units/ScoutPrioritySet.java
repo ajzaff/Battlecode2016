c2c416f5aba4586444da817658756632c3df1077
Alan Zaffetti <azco21@cox.net>
2016-01-15 22:21:58 -0500

    scout PS

---
+++ ai/actions/priority/units/ScoutPrioritySet.java
@@ -0,0 +1,4 @@
+package team137.ai.actions.priority.units;
+
+public class ScoutPrioritySet extends MovablePrioritySet {
+}
--- ai/units/Scout.java
+++ ai/units/Scout.java
@@ -1,10 +1,15 @@
 package team137.ai.units;
 
 import battlecode.common.RobotController;
+import battlecode.common.Team;
 
 public class Scout extends MovableUnit {
+
+  private final Team team;
+
   public Scout(RobotController rc) {
     super(rc);
+    team = rc.getTeam();
   }
 
   @Override
